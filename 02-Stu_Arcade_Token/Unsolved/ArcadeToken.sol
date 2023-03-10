1e76fcebd4770824c685295af9120c42c13d339a
scoopcash <114896185+scoopcash@users.noreply.github.com>
2023-03-09 20:14:11 -0500

    done ex1

--- 02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
+++ 02-Stu_Arcade_Token/Unsolved/ArcadeToken.sol
@@ -3,3 +3,37 @@ Arcade Token
 */
 
 // Construct your ArcadeToken contract below:
+
+pragma solidity ^0.5.0;
+
+contract AcradeToken {
+
+
+    address payable owner = msg.sender;
+    string public symbol = "ARCD";
+    uint public exchangeRate = 100;
+
+    mapping(address => uint) balances; //accepts an account address as key and account balance as value. where the value is an uint
+
+    function balance() public view returns(uint){
+        return balances[msg.sender];
+    }
+
+    function transfer(address recipient, uint value) public{
+        balances[msg.sender] -= value;
+        balances[recipient] += value;
+    }
+
+    function purchase() public payable {
+        uint amount = msg.value * exchangeRate;
+        balances[msg.sender] += amount;
+        owner.transfer(msg.value);
+    } 
+
+    function mint(address recipient, uint value) public {
+        require(msg.sender == owner, "you are not allowed to mint tokens");
+        balances[recipient] += value;
+    }
+
+
+}
